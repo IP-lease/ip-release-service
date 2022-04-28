12396df765b253c034797e25071338ff91d6c911
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-28 11:38:57 +0900

    [REMOVE] 불필요한 클래스 제거

--- src/main/kotlin/com/iplease/server/ip/release/global/Permission.kt
+++
@@ -1,7 +0,0 @@
-package com.iplease.server.ip.release.global
-
-enum class Permission {
-    IP_RELEASE_DEMAND, IP_RELEASE_DEMAND_CANCEL,
-    IP_RELEASE_RESERVE, IP_RELEASE_RESERVE_CANCEL,
-    IP_RELEASE_ACCEPT
-}
