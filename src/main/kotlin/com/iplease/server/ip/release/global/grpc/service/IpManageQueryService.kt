a696669c27e4a81e98e281beaf3dfd5255510b46
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-30 21:16:16 +0900

    [FIX] 전역 서비스에 대한 인터페이스 위치 수정

--- src/main/kotlin/com/iplease/server/ip/release/global/grpc/service/IpManageQueryService.kt
+++
@@ -1,9 +0,0 @@
-package com.iplease.server.ip.release.global.grpc.service
-
-import com.iplease.server.ip.release.global.grpc.dto.AssignedIp
-
-//TODO gRPC 도입예정
-interface IpManageQueryService {
-    fun existsAssignedIpByUuid(uuid: Long): Boolean
-    fun getAssignedIpByUuid(uuid: Long): AssignedIp
-}
