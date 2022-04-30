b224cff0c9ee949f228238f7a882a8d169cab798
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-30 19:40:00 +0900

    [ADD] proto-lib 모듈 추가 및 proto 파일을 기반으로한 grpc proto lib 0.0.1 SNAPSHOT 배포

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,9 @@ repositories {
 extra["springCloudVersion"] = "2021.0.1"
 
 dependencies {
+    implementation(files("libs/proto-lib-0.0.1-SNAPSHOT.jar"))
     implementation("org.springframework.boot:spring-boot-starter-amqp")
-    implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -37,18 +38,32 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation ("org.springdoc:springdoc-openapi-webflux-ui:1.6.7")
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.7")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.mapstruct:mapstruct:1.4.1.Final")
+    implementation("org.mapstruct:mapstruct:1.4.2.Final")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.0")
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("dev.miku:r2dbc-mysql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.4.1.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
-    testImplementation ("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    
+    implementation("com.linecorp.armeria:armeria")
+    implementation("com.linecorp.armeria:armeria-grpc:1.16.0")
+    implementation("com.salesforce.servicelibs:reactor-grpc-stub:1.2.3")
+    implementation("io.projectreactor:reactor-core:3.4.16")
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation("com.linecorp.armeria:armeria-junit5:1.16.0")
+    implementation("io.grpc:grpc-protobuf:1.45.1")
+    implementation("io.grpc:grpc-stub:1.45.1")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:2.0.0")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
+    runtimeOnly("org.slf4j:log4j-over-slf4j:1.7.36")
+    implementation("com.linecorp.armeria:armeria-spring-boot-webflux-starter:0.99.7")
 }
 
 dependencyManagement {
