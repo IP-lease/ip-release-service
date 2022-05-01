e84865155b2fdd733f5cab9b4e89bee49fd54b86
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-01 19:52:47 +0900

    [ADD] IpManageQueryService 에 대한 Grpc구현체 작성

---
+++ src/main/kotlin/com/iplease/server/ip/release/domain/request/service/GrpcIpManageQueryService.kt
@@ -0,0 +1,35 @@
+package com.iplease.server.ip.release.domain.request.service
+
+import com.google.protobuf.Int64Value
+import com.google.protobuf.Timestamp
+import com.iplease.lib.ip.release.AssignedIp
+import com.iplease.lib.ip.release.ReactorIpManageQueryServiceGrpc.ReactorIpManageQueryServiceStub
+import com.iplease.server.ip.release.domain.request.data.dto.AssignedIpDto
+import com.iplease.server.ip.release.global.request.service.IpManageQueryService
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+@Service
+class GrpcIpManageQueryService(
+    private val reactorIpManageQueryServiceStub: ReactorIpManageQueryServiceStub
+): IpManageQueryService {
+    override fun existsAssignedIpByUuid(uuid: Mono<Long>): Mono<Boolean> =
+        uuid.map { Int64Value.newBuilder().setValue(it).build() }
+            .let { reactorIpManageQueryServiceStub.existsAssignedIpByUuid(it) }
+            .map { it.value }
+
+    override fun getAssignedIpByUuid(uuid: Mono<Long>): Mono<AssignedIpDto> =
+        uuid.map { Int64Value.newBuilder().setValue(it).build() }
+            .let { reactorIpManageQueryServiceStub.getAssignedIpByUuid(it) }
+            .map { it.toAssignedIpDto() }
+
+    private fun AssignedIp.toAssignedIpDto()= AssignedIpDto(uuid, issuerUuid, assignerUuid, assignedAt.toLocalDateTime())
+    private fun Timestamp.toLocalDateTime(): LocalDateTime =
+        Instant.ofEpochSecond(seconds, nanos.toLong())
+            .atZone( ZoneId.of( "Asia/Seoul" ) )
+            .toLocalDateTime()
+}
+
