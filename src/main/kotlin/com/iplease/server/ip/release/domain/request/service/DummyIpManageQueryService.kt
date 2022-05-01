b9f3235f391ab44f6c23cec728a0034b3938b106
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-01 19:44:35 +0900

    [FIX] IpManageQueryService 스펙 변경 및 적용

--- src/main/kotlin/com/iplease/server/ip/release/domain/request/service/DummyIpManageQueryService.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/request/service/DummyIpManageQueryService.kt
@@ -2,11 +2,11 @@ package com.iplease.server.ip.release.domain.request.service
 
 import com.iplease.server.ip.release.domain.request.data.dto.AssignedIpDto
 import com.iplease.server.ip.release.global.request.service.IpManageQueryService
-import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import java.time.LocalDateTime
 
-@Service //TODO 나중에 gRPC 적용 후 비활성화
 class DummyIpManageQueryService: IpManageQueryService {
-    override fun existsAssignedIpByUuid(uuid: Long): Boolean = true
-    override fun getAssignedIpByUuid(uuid: Long): AssignedIpDto = AssignedIpDto(uuid, 0L, 0L, LocalDateTime.MIN)
+    override fun existsAssignedIpByUuid(uuid: Mono<Long>) = true.toMono()
+    override fun getAssignedIpByUuid(uuid: Mono<Long>) = uuid.map{ AssignedIpDto(it, 0L, 0L, LocalDateTime.MIN) }
 }
--- src/main/kotlin/com/iplease/server/ip/release/global/request/service/IpManageQueryService.kt
+++ src/main/kotlin/com/iplease/server/ip/release/global/request/service/IpManageQueryService.kt
@@ -1,9 +1,10 @@
 package com.iplease.server.ip.release.global.request.service
 
 import com.iplease.server.ip.release.domain.request.data.dto.AssignedIpDto
+import reactor.core.publisher.Mono
 
 //TODO gRPC 도입예정
 interface IpManageQueryService {
-    fun existsAssignedIpByUuid(uuid: Long): Boolean
-    fun getAssignedIpByUuid(uuid: Long): AssignedIpDto
+    fun existsAssignedIpByUuid(uuid: Mono<Long>): Mono<Boolean>
+    fun getAssignedIpByUuid(uuid: Mono<Long>): Mono<AssignedIpDto>
 }
