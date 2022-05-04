c153a623679295a5cc5e5751d6306198668fb51f
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-04 12:25:15 +0900

    [ADD] SimplePolicyCheckService 로 기존 컨트롤러에 존재했던 정책검사 함수 이전 및 PolicyCheckService 적용

--- src/main/kotlin/com/iplease/server/ip/release/domain/demand/controller/IpReleaseDemandController.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/demand/controller/IpReleaseDemandController.kt
@@ -1,16 +1,12 @@
 package com.iplease.server.ip.release.domain.demand.controller
 
 import com.iplease.server.ip.release.domain.demand.data.response.DemandReleaseIpResponse
-import com.iplease.server.ip.release.domain.demand.exception.*
 import com.iplease.server.ip.release.domain.demand.service.IpReleaseDemandService
-import com.iplease.server.ip.release.global.demand.exception.UnknownDemandException
 import com.iplease.server.ip.release.global.event.service.EventPublishService
 import com.iplease.server.ip.release.global.event.type.Event
 import com.iplease.server.ip.release.global.common.type.Permission
 import com.iplease.server.ip.release.global.common.type.Role
-import com.iplease.server.ip.release.global.demand.service.IpManageQueryService
-import com.iplease.server.ip.release.global.demand.service.IpReleaseDemandQueryService
-import com.iplease.server.ip.release.global.common.exception.PermissionDeniedException
+import com.iplease.server.ip.release.global.policy.service.PolicyCheckService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -19,73 +15,39 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping("/api/v1/ip/release/demand")
 class IpReleaseDemandController(
     private val ipReleaseDemandService: IpReleaseDemandService,
-    private val ipReleaseDemandQueryService: IpReleaseDemandQueryService,
-    private val ipManageQueryService: IpManageQueryService,
-    private val eventPublishService: EventPublishService
+    private val eventPublishService: EventPublishService,
+    private val policyCheckService: PolicyCheckService
 ) {
     //IP 할당 해제 신청
-    @PostMapping("/{assignedIpUuid}")
+    @PostMapping("/{assignedIpUuid}") //TODO PathVariable to Payload 전환 고려해보기
     fun demandReleaseIp(@PathVariable assignedIpUuid: Long,
                         @RequestHeader("X-Login-Account-Uuid") issuerUuid: Long,
                         @RequestHeader("X-Login-Account-Role") role: Role
     ): Mono<ResponseEntity<DemandReleaseIpResponse>> =
-        checkPermission(role, Permission.IP_RELEASE_DEMAND)
-            .flatMap { checkAssignedIpExists(assignedIpUuid) }
-            .flatMap { checkAssignedIpAccess(assignedIpUuid, issuerUuid) }
-            .flatMap { ipReleaseDemandService.demand(assignedIpUuid, issuerUuid) }
+        policyCheckService.run {
+            checkPermission(role, Permission.IP_RELEASE_DEMAND)
+                .flatMap { checkAssignedIpExists(assignedIpUuid) }
+                .flatMap { checkAssignedIpAccess(assignedIpUuid, issuerUuid) }
+        }.flatMap { ipReleaseDemandService.demand(assignedIpUuid, issuerUuid) }
             .map { eventPublishService.publish(Event.IP_RELEASE_DEMAND_ADD.routingKey, it) }
-            .map {
-                DemandReleaseIpResponse(it.uuid, it.assignedIpUuid, it.issuerUuid, it.status)
-                    .let { response -> ResponseEntity.ok(response) } }
+            .map { DemandReleaseIpResponse(it.uuid, it.assignedIpUuid, it.issuerUuid, it.status) }
+            .map { ResponseEntity.ok(it) }
 
     //IP 할당 해제 신청 취소
-    @DeleteMapping("/{uuid}")
-    fun cancelDemandReleaseIp(@PathVariable uuid: Long,
+    @DeleteMapping("/{demandUuid}")
+    fun cancelDemandReleaseIp(@PathVariable demandUuid: Long,
                               @RequestHeader("X-Login-Account-Uuid") issuerUuid: Long,
                               @RequestHeader("X-Login-Account-Role") role: Role
     ): Mono<ResponseEntity<Unit>> =
-        checkPermission(role, Permission.IP_RELEASE_DEMAND_CANCEL)
-            .flatMap { checkDemandExists(uuid) }
-            .flatMap { checkDemandAccess(uuid, issuerUuid) }
-            .flatMap { ipReleaseDemandService.cancel(uuid, issuerUuid) }
+        policyCheckService.run {
+            checkPermission(role, Permission.IP_RELEASE_DEMAND_CANCEL)
+                .flatMap { checkDemandExists(demandUuid) }
+                .flatMap { checkDemandAccess(demandUuid, issuerUuid) }
+        }.flatMap { ipReleaseDemandService.cancel(demandUuid, issuerUuid) }
             .map { ResponseEntity.ok(it) }
-
-
-    private fun checkDemandExists(uuid: Long) =
-        ipReleaseDemandQueryService.existsDemandByUuid(uuid.toMono())
-            .flatMap {
-                if(it) Mono.just(Unit)
-                else Mono.defer { Mono.error(UnknownDemandException(uuid)) }
-            }
-
-    private fun checkDemandAccess(uuid: Long, issuerUuid: Long) =
-        ipReleaseDemandQueryService.getDemandByUuid(uuid.toMono())
-            .map { it.issuerUuid }
-            .flatMap {
-                if(it == issuerUuid) Mono.just(Unit)
-                else Mono.defer { Mono.error(WrongAccessDemandException(uuid, issuerUuid)) }
-            }
-
-    private fun checkPermission(role: Role, permission: Permission) =
-        if (role.hasPermission(permission)) Mono.just(Unit)
-        else Mono.defer { Mono.error(PermissionDeniedException(permission)) }
-
-    private fun checkAssignedIpExists(assignedIpUuid: Long) =
-        ipManageQueryService.existsAssignedIpByUuid(assignedIpUuid.toMono()).flatMap {
-            if (it) Mono.just(Unit)
-            else Mono.defer { Mono.error(UnknownAssignedIpException(assignedIpUuid)) }
-        }
-
-    private fun checkAssignedIpAccess(assignedIpUuid: Long, issuerUuid: Long) =
-        ipManageQueryService.getAssignedIpByUuid(assignedIpUuid.toMono())
-            .flatMap {
-                if(it.issuerUuid == issuerUuid) Mono.just(Unit)
-                else Mono.defer { Mono.error(WrongAccessAssignedIpException(assignedIpUuid, issuerUuid)) }
-            }
 }
--- src/test/kotlin/com/iplease/server/ip/release/domain/admin/controller/IpReleaseAdminControllerTest.kt
+++ src/test/kotlin/com/iplease/server/ip/release/domain/admin/controller/IpReleaseAdminControllerTest.kt
@@ -2,18 +2,16 @@ package com.iplease.server.ip.release.domain.admin.controller
 
 import com.iplease.server.ip.release.domain.admin.dto.IpReleaseAcceptDto
 import com.iplease.server.ip.release.domain.admin.service.IpReleaseAdminService
-import com.iplease.server.ip.release.global.demand.exception.UnknownDemandException
 import com.iplease.server.ip.release.global.demand.service.IpReleaseDemandQueryService
 import com.iplease.server.ip.release.global.event.service.EventPublishService
 import com.iplease.server.ip.release.global.event.type.Event
-import com.iplease.server.ip.release.global.common.exception.PermissionDeniedException
-import com.iplease.server.ip.release.global.common.type.Permission
 import com.iplease.server.ip.release.global.common.type.Role
+import com.iplease.server.ip.release.global.policy.service.PolicyCheckService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.*
+import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import kotlin.properties.Delegates
 import kotlin.random.Random
@@ -23,28 +21,37 @@ class IpReleaseAdminControllerTest {
     private lateinit var ipReleaseAdminService: IpReleaseAdminService
     private lateinit var ipReleaseDemandQueryService: IpReleaseDemandQueryService
     private lateinit var eventPublishService: EventPublishService
+    private lateinit var policyCheckService: PolicyCheckService
     private var demandUuid by Delegates.notNull<Long>()
     private var operatorUuid by Delegates.notNull<Long>()
+    private val monoJustAny = Mono.just(Any())
     @BeforeEach
     fun setUp() {
         ipReleaseAdminService = mock()
         ipReleaseDemandQueryService = mock()
         eventPublishService = mock()
+        policyCheckService = mock() {
+            on { checkDemandExists(any()) } doReturn monoJustAny
+            on { checkDemandAccess(any(), any()) } doReturn monoJustAny
+            on { checkPermission(any(), any()) } doReturn monoJustAny
+            on { checkAssignedIpExists(any())} doReturn monoJustAny
+            on { checkAssignedIpAccess(any(), any()) } doReturn monoJustAny
+        }
 
-        target = IpReleaseAdminController(ipReleaseAdminService, ipReleaseDemandQueryService, eventPublishService)
+        target = IpReleaseAdminController(ipReleaseAdminService, eventPublishService, policyCheckService)
 
         demandUuid = Random.nextLong()
         operatorUuid = Random.nextLong()
     }
 
     //IP 할당 해제 수락 조건
-    //IP_RELEASE_ACCEPT 권한이 있어야하며,
-    //수락할 신청이 존재해야하며
+    //IP_RELEASE_ACCEPT 권한이 있어야하며, -> PolicyCheckService 에 위임
+    //수락할 신청이 존재해야하며 -> PolicyCheckService 에 위임
     //해당 신청의 상태가 수락가능한(Acceptable) 상태여야한다. -> Service 단에 위임
 
     //IP 할당 해제 수락시
     //해당 Demand를 수락(삭제)시키고 -> Service 단에 위임
-    // IP할당해제 이벤트를 발행한다.
+    // IP할당해제 이벤트를 발행한다. -> EventPublishService 에 위임
     @Test @DisplayName("IP 할당 해제 수락 - 수락 성공")
     fun acceptReleaseIpSuccess() {
         val dto = IpReleaseAcceptDto(demandUuid, operatorUuid)
@@ -57,31 +64,5 @@ class IpReleaseAdminControllerTest {
 
         verify(ipReleaseAdminService, times(1)).acceptDemand(demandUuid, operatorUuid)
         verify(eventPublishService, times(1)).publish(Event.IP_RELEASED.routingKey, dto)
-    }
-
-    @Test @DisplayName("IP 할당 해제 수락 - 권한이 없을경우")
-   fun acceptReleaseIpFailurePermissionDenied() {
-        val dto = IpReleaseAcceptDto(demandUuid, operatorUuid)
-
-        whenever(ipReleaseDemandQueryService.existsDemandByUuid(any())).thenReturn(true.toMono())
-
-       val exception = assertThrows<PermissionDeniedException> { target.acceptReleaseIp(demandUuid, operatorUuid, Role.GUEST).block()!! }
-
-        assert(exception.permission == Permission.IP_RELEASE_ACCEPT)
-        verify(ipReleaseAdminService, times(0)).acceptDemand(demandUuid, operatorUuid)
-        verify(eventPublishService, times(0)).publish(Event.IP_RELEASED.routingKey, dto)
-    }
-
-    @Test @DisplayName("IP 할당 해제 수락 - 신청이 존재하지 않을경우")
-    fun acceptReleaseIpFailureDemandNotExist() {
-        val dto = IpReleaseAcceptDto(demandUuid, operatorUuid)
-
-        whenever(ipReleaseDemandQueryService.existsDemandByUuid(any())).thenReturn(false.toMono())
-
-        val exception = assertThrows<UnknownDemandException> { target.acceptReleaseIp(demandUuid, operatorUuid, Role.ADMINISTRATOR).block()!! }
-
-        assert(exception.uuid == demandUuid)
-        verify(ipReleaseAdminService, times(0)).acceptDemand(demandUuid, operatorUuid)
-        verify(eventPublishService, times(0)).publish(Event.IP_RELEASED.routingKey, dto)
     }
 }
