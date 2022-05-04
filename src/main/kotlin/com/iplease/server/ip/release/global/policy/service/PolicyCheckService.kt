48029b4636f47133171bb70174b2fdadb1d51166
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-04 12:18:22 +0900

    [ADD] 리펙터링을 위해 PolicyCheckService 인터페이스 작성

---
+++ src/main/kotlin/com/iplease/server/ip/release/global/policy/service/PolicyCheckService.kt
@@ -0,0 +1,13 @@
+package com.iplease.server.ip.release.global.policy.service
+
+import com.iplease.server.ip.release.global.common.type.Permission
+import com.iplease.server.ip.release.global.common.type.Role
+import reactor.core.publisher.Mono
+
+interface PolicyCheckService {
+    fun checkPermission(role: Role, permission: Permission): Mono<Any>
+    fun checkDemandExists(demandUuid: Long): Mono<Any>
+    fun checkDemandAccess(demandUuid: Long, accessorUuid: Long): Mono<Any>
+    fun checkAssignedIpExists(assignedIpUuid: Long): Mono<Any>
+    fun checkAssignedIpAccess(assignedIpUuid: Long, accessorUuid: Long): Mono<Any>
+}
