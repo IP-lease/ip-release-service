1cf221e4e6c7d891309804a661481376a74710de
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-05 23:54:59 +0900

    [ADD] Repository 에 서비스로직에 필요한 쿼리 메서드 작성

--- src/main/kotlin/com/iplease/server/ip/release/domain/reserve/repository/IpReleaseReserveRepository.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/reserve/repository/IpReleaseReserveRepository.kt
@@ -2,6 +2,8 @@ package com.iplease.server.ip.release.domain.reserve.repository
 
 import com.iplease.server.ip.release.domain.reserve.data.table.IpReleaseReserveTable
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import reactor.core.publisher.Mono
 
 interface IpReleaseReserveRepository: R2dbcRepository<IpReleaseReserveTable, Long> {
+    fun existsByAssignedIpUuid(assignedIpUuid: Long): Mono<Boolean>
 }
