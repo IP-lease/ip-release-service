19a95edb7a006de20faba2fddb166d051589bc90
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-05 21:41:27 +0900

    [FIX] 누락된 해제일 추가를 위해 Service 인터페이스 스펙 변경

--- src/main/kotlin/com/iplease/server/ip/release/domain/reserve/service/IpReleaseReserveService.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/reserve/service/IpReleaseReserveService.kt
@@ -2,7 +2,8 @@ package com.iplease.server.ip.release.domain.reserve.service
 
 import com.iplease.server.ip.release.domain.reserve.data.dto.IpReleaseReserveDto
 import reactor.core.publisher.Mono
+import java.time.LocalDate
 
 interface IpReleaseReserveService {
-    fun reserve(assignedIpUuid: Long, issuerUuid: Long): Mono<IpReleaseReserveDto>
+    fun reserve(assignedIpUuid: Long, issuerUuid: Long, releaseAt: LocalDate): Mono<IpReleaseReserveDto>
 }
