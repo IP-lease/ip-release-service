9f73cb5b16b3d25774dbf8bb818af7ba1dd6e7f5
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-08 11:12:50 +0900

    [FIX] ReserveController - reserve 로깅 로직 분리

--- src/main/kotlin/com/iplease/server/ip/release/domain/reserve/controller/IpReleaseReserveController.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/reserve/controller/IpReleaseReserveController.kt
@@ -4,8 +4,10 @@ import com.iplease.server.ip.release.domain.reserve.data.response.ReserveRelease
 import com.iplease.server.ip.release.domain.reserve.service.IpReleaseReserveService
 import com.iplease.server.ip.release.global.common.data.type.Permission
 import com.iplease.server.ip.release.global.common.data.type.Role
+import com.iplease.server.ip.release.global.log.service.LoggingService
+import com.iplease.server.ip.release.global.log.type.LoggingActType
+import com.iplease.server.ip.release.global.log.util.ReserveRequestInput
 import com.iplease.server.ip.release.global.policy.service.PolicyCheckService
-import org.slf4j.LoggerFactory
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -17,45 +19,22 @@ import java.time.LocalDate
 @RequestMapping("/ip/release/reserve")
 class IpReleaseReserveController(
     private val ipReleaseReserveService: IpReleaseReserveService,
-    private val policyCheckService: PolicyCheckService
+    private val policyCheckService: PolicyCheckService,
+    private val loggingService: LoggingService
 ) {
-    val LOGGER = LoggerFactory.getLogger(this::class.java)
-    val CONTROLLER_PREFIX = "[RESTAPI]"
-
     @PostMapping("/{assignedIpUuid}")
     fun reserveReleaseIp(@PathVariable assignedIpUuid: Long,
                          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) releaseAt: LocalDate,
                          @RequestHeader("X-Login-Account-Uuid") issuerUuid: Long,
                          @RequestHeader("X-Login-Account-Role") role: Role
     ): Mono<ResponseEntity<ReserveReleaseIpResponse>> =
-        logReserveRequestStart(assignedIpUuid, releaseAt, issuerUuid, role)
-            .flatMap { policyCheckService.checkPermission(role, Permission.IP_RELEASE_RESERVE) }
+        policyCheckService.checkPermission(role, Permission.IP_RELEASE_RESERVE)
             .flatMap { policyCheckService.checkAssignedIpExists(assignedIpUuid) }
             .flatMap { policyCheckService.checkAssignedIpAccess(assignedIpUuid, issuerUuid) }
             .flatMap { ipReleaseReserveService.reserve(assignedIpUuid, issuerUuid, releaseAt) }
             .map { ReserveReleaseIpResponse(it.uuid, it.assignedIpUuid, it.issuerUuid, it.releaseAt) }
             .map { ResponseEntity.ok(it) }
-            .doOnSuccess { logReserveRequestComplete(it.body!!) }
-            .doOnError{ logReserveRequestError(it) }
-
-    private fun logReserveRequestStart(assignedIpUuid: Long, releaseAt: LocalDate, issuerUuid: Long, role: Role): Mono<Unit> {
-        val PREFIX = "$CONTROLLER_PREFIX [해제예약 - 등록]"
-        LOGGER.info("$PREFIX 예약등록을 진행합니다.")
-        LOGGER.info("$PREFIX     요청 정보: $assignedIpUuid, $releaseAt, $issuerUuid, $role")
-        return Unit.toMono()
-    }
-
-    private fun logReserveRequestComplete(response: ReserveReleaseIpResponse) {
-        val PREFIX = "$CONTROLLER_PREFIX [해제예약 - 등록]"
-        LOGGER.warn("$PREFIX     예약등록을 완료하였습니다.")
-        LOGGER.warn("$PREFIX     응답 정보: $response")
-    }
-
-    private fun logReserveRequestError(throwable: Throwable) {
-        val PREFIX = "$CONTROLLER_PREFIX [해제예약 - 등록]"
-        LOGGER.warn("$PREFIX     예약등록중 오류가 발생하였습니다!")
-        LOGGER.warn("$PREFIX     오류 내용: ${throwable.message}")
-    }
+            .let { loggingService.withLog(ReserveRequestInput(assignedIpUuid, releaseAt, issuerUuid, role), it, LoggingActType.RESERVE_REQUEST_LOGGER) }
 
     @DeleteMapping("/{reserveUuid}")
     fun cancelReserveReleaseIp(@PathVariable reserveUuid: Long,
--- src/main/kotlin/com/iplease/server/ip/release/global/log/service/LoggingServiceImpl.kt
+++ src/main/kotlin/com/iplease/server/ip/release/global/log/service/LoggingServiceImpl.kt
@@ -1,23 +1,22 @@
 package com.iplease.server.ip.release.global.log.service
 
 import com.iplease.server.ip.release.global.log.type.LoggingActType
-import com.iplease.server.ip.release.global.log.util.ReserveJobDemandLoggerUtil
-import com.iplease.server.ip.release.global.log.util.LoggerUtil
-import com.iplease.server.ip.release.global.log.util.ReserveJobDeleteLoggerUtil
-import com.iplease.server.ip.release.global.log.util.ReserveSchedulerLoggerUtil
+import com.iplease.server.ip.release.global.log.util.*
 import org.springframework.stereotype.Service
 
 @Service
 class LoggingServiceImpl(
     private val reserveJobDemandLoggerUtil: ReserveJobDemandLoggerUtil,
     private val reserveJobDeleteLoggerUtil: ReserveJobDeleteLoggerUtil,
-    private val reserveSchedulerLoggerUtil: ReserveSchedulerLoggerUtil
+    private val reserveSchedulerLoggerUtil: ReserveSchedulerLoggerUtil,
+    private val reserveRequestLoggerUtil: ReserveRequestLoggerUtil
 ): LoggingService {
     override fun <IN, OUT> getLoggerUtil(type: LoggingActType): LoggerUtil<IN, OUT> =
         when (type) {
             LoggingActType.RESERVE_JOB_DEMAND_LOGGER -> reserveJobDemandLoggerUtil
             LoggingActType.RESERVE_JOB_DELETE_LOGGER -> reserveJobDeleteLoggerUtil
             LoggingActType.RESERVE_SCHEDULER_LOGGER -> reserveSchedulerLoggerUtil
+            LoggingActType.RESERVE_REQUEST_LOGGER -> reserveRequestLoggerUtil
             else -> throw IllegalArgumentException("LoggingActType is not supported.")
         } as LoggerUtil<IN, OUT>
 }
