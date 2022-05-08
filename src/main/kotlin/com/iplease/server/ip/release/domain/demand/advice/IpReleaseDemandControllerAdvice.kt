6a37e296fceee5c3c7f20dfb894a71de44ea2aeb
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-08 12:08:23 +0900

    [FIX] Advice 로직 오류 수정

--- src/main/kotlin/com/iplease/server/ip/release/domain/demand/advice/IpReleaseDemandControllerAdvice.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/demand/advice/IpReleaseDemandControllerAdvice.kt
@@ -11,12 +11,6 @@ import reactor.kotlin.core.publisher.toMono
 
 @RestControllerAdvice(basePackageClasses = [IpReleaseDemandController::class])
 class IpReleaseDemandControllerAdvice {
-    @ExceptionHandler(AlreadyDemandedAssignedIpException::class)
-    fun handle(exception: AlreadyDemandedAssignedIpException) =
-        ErrorResponse(ErrorCode.ALREADY_DEMANDED, "이미 신청하셧습니다!", "이미 해당 할당IP에 대한 해제신청이 진행중입니다.")
-            .let { ResponseEntity.badRequest().body(it) }
-            .let { it.toMono() }
-
     @ExceptionHandler(NotCancelableDemandException::class)
     fun handle(exception: NotCancelableDemandException) =
         ErrorResponse(ErrorCode.UN_CANCELABLE_DEMAND, "취소할 수 없는 신청입니다!", "해당신청의 상태를 재확인해주세요.")
--- src/main/kotlin/com/iplease/server/ip/release/global/demand/advice/GlobalControllerDemandAdvice.kt
+++ src/main/kotlin/com/iplease/server/ip/release/global/demand/advice/GlobalControllerDemandAdvice.kt
@@ -2,6 +2,7 @@ package com.iplease.server.ip.release.global.demand.advice
 
 import com.iplease.server.ip.release.global.common.data.response.ErrorResponse
 import com.iplease.server.ip.release.domain.demand.data.type.ErrorCode
+import com.iplease.server.ip.release.domain.demand.exception.AlreadyDemandedAssignedIpException
 import com.iplease.server.ip.release.global.demand.exception.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -34,5 +35,11 @@ class GlobalControllerDemandAdvice {
     fun handle(exception: WrongAccessDemandException) =
         ErrorResponse(ErrorCode.WRONG_ACCESS_DEMAND, "해제신청에 접근할 수 없습니다!", "해제신청을 소유하고있는지 확인해주세요.")
             .let { ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(it) }
+            .let { it.toMono() }
+
+    @ExceptionHandler(AlreadyDemandedAssignedIpException::class)
+    fun handle(exception: AlreadyDemandedAssignedIpException) =
+        ErrorResponse(ErrorCode.ALREADY_DEMANDED, "이미 신청하셧습니다!", "이미 해당 할당IP에 대한 해제신청이 진행중입니다.")
+            .let { ResponseEntity.badRequest().body(it) }
             .let { it.toMono() }
 }
