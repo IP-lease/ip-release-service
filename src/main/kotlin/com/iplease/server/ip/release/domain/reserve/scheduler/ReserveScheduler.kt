2002561a9c30d7c16d7a9897dd5f8e207a4b7b59
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-08 10:53:05 +0900

    [FIX] ReserveScheduler - executeReserve 로깅 로직 분리

--- src/main/kotlin/com/iplease/server/ip/release/domain/reserve/scheduler/ReserveScheduler.kt
+++ src/main/kotlin/com/iplease/server/ip/release/domain/reserve/scheduler/ReserveScheduler.kt
@@ -1,22 +1,20 @@
 package com.iplease.server.ip.release.domain.reserve.scheduler
 
 import com.iplease.server.ip.release.domain.reserve.scheduler.job.ReserveJob
-import org.slf4j.LoggerFactory
+import com.iplease.server.ip.release.global.log.service.LoggingService
+import com.iplease.server.ip.release.global.log.type.LoggingActType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import reactor.kotlin.core.publisher.toMono
 
 @Component
 class ReserveScheduler(
-    private val reserveJob: ReserveJob
+    private val reserveJob: ReserveJob,
+    private val loggingService: LoggingService
 ) {
-    val LOGGER = LoggerFactory.getLogger(this::class.java)
-    val SCHEDULE_PREFIX = "[SCHEDULE]"
     @Scheduled(cron = "0 0 0,6,12,18 * * ?")
     fun executeReserve() {
-        val PREFIX = "$SCHEDULE_PREFIX [해제예약 - 당일예약된 해제신청 등록]"
-        LOGGER.info("$PREFIX 스케줄을 시작합니다.").toMono()
-            .flatMap { reserveJob.reserveAtToday().collectList() }
-            .subscribe{ LOGGER.info("$PREFIX 스케줄을 완료하였습니다.") }
+        reserveJob.reserveAtToday().collectList()
+            .let { loggingService.withLog(Unit, it, LoggingActType.RESERVE_SCHEDULER_LOGGER) }
+            .subscribe()
     }
 }
--- src/main/kotlin/com/iplease/server/ip/release/global/log/service/LoggingServiceImpl.kt
+++ src/main/kotlin/com/iplease/server/ip/release/global/log/service/LoggingServiceImpl.kt
@@ -4,17 +4,20 @@ import com.iplease.server.ip.release.global.log.type.LoggingActType
 import com.iplease.server.ip.release.global.log.util.ReserveJobDemandLoggerUtil
 import com.iplease.server.ip.release.global.log.util.LoggerUtil
 import com.iplease.server.ip.release.global.log.util.ReserveJobDeleteLoggerUtil
+import com.iplease.server.ip.release.global.log.util.ReserveSchedulerLoggerUtil
 import org.springframework.stereotype.Service
 
 @Service
 class LoggingServiceImpl(
     private val reserveJobDemandLoggerUtil: ReserveJobDemandLoggerUtil,
-    private val reserveJobDeleteLoggerUtil: ReserveJobDeleteLoggerUtil
+    private val reserveJobDeleteLoggerUtil: ReserveJobDeleteLoggerUtil,
+    private val reserveSchedulerLoggerUtil: ReserveSchedulerLoggerUtil
 ): LoggingService {
     override fun <IN, OUT> getLoggerUtil(type: LoggingActType): LoggerUtil<IN, OUT> =
         when (type) {
             LoggingActType.RESERVE_JOB_DEMAND_LOGGER -> reserveJobDemandLoggerUtil
             LoggingActType.RESERVE_JOB_DELETE_LOGGER -> reserveJobDeleteLoggerUtil
+            LoggingActType.RESERVE_SCHEDULER_LOGGER -> reserveSchedulerLoggerUtil
             else -> throw IllegalArgumentException("LoggingActType is not supported.")
         } as LoggerUtil<IN, OUT>
 }
