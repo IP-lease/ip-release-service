bf63fbc674a47779b32503b69e2722af7bd13835
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-14 14:05:04 +0900

    [ADD] ClientLoadBalancing 을 적용한 WebClientBuilder를 빈으로 등록

--- src/main/kotlin/com/iplease/server/ip/release/IpReleaseServiceApplication.kt
+++ src/main/kotlin/com/iplease/server/ip/release/IpReleaseServiceApplication.kt
@@ -3,12 +3,20 @@ package com.iplease.server.ip.release
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
-class IpReleaseServiceApplication
+class IpReleaseServiceApplication {
+    @Bean
+    fun webClientBuilder(function: ReactorLoadBalancerExchangeFilterFunction): WebClient.Builder =
+        WebClient.builder()
+            .filter(function)
+}
 
 fun main(args: Array<String>) {
     runApplication<IpReleaseServiceApplication>(*args)
