20bbdba7b73977e4ec931dbd1f0d1773386ace1c
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-14 14:04:35 +0900

    [FIX] GrpcConfig 에서 Stub 활성화

--- src/main/kotlin/com/iplease/server/ip/release/infra/grpc/config/GrpcConfig.kt
+++ src/main/kotlin/com/iplease/server/ip/release/infra/grpc/config/GrpcConfig.kt
@@ -1,19 +1,20 @@
 package com.iplease.server.ip.release.infra.grpc.config
 
-import com.iplease.lib.ip.release.ReactorIpManageQueryServiceGrpc.ReactorIpManageQueryServiceStub
-import com.linecorp.armeria.client.Clients.newClient
+import com.iplease.lib.ip.manage.ReactorIpManageQueryServiceGrpc.ReactorIpManageQueryServiceStub
+import com.linecorp.armeria.client.grpc.GrpcClients
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class GrpcConfig(
     val discoveryClient: DiscoveryClient
 ) {
-    //todo @Bean
-    fun ipManageQueryService(): ReactorIpManageQueryServiceStub =
+    @Bean
+    fun reactorIpManageQueryServiceStub(): ReactorIpManageQueryServiceStub =
         //TODO 나중에 scale out 으로 ip manage server 에 대한 CloudLoadbalancing 이 필요할 경우 추천하지 않는 방법
         discoveryClient.getInstances("ip-manage-server")
             .let { if (it.isEmpty()) throw RuntimeException("ip-manage-server not found") else it }
-            .let { newClient(it[0].uri, ReactorIpManageQueryServiceStub::class.java) }
-
+            .let { return@let it[0].uri }
+            .let { GrpcClients.newClient(it, ReactorIpManageQueryServiceStub::class.java) }
 }
